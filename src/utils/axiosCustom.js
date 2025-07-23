86501e13a3813f2fcff4b283e733c2ffda4e2b9f
qcuong57 <hoquoccuong945@gmail.com>
2025-07-23 19:19:11 +0700

    fix axios

--- src/utils/axiosCustom.js
+++ src/utils/axiosCustom.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "./token";
 
 const axiosCustom = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
